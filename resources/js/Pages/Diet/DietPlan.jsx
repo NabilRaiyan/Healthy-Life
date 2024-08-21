4e0570656249f828e2c2af4a24bf1792accaceab
NabilRaiyan <rraiyan77@gmail.com>
2024-08-21 16:28:52 +0600

    creating a chatbot

--- resources/js/Pages/Diet/DietPlan.jsx
+++ resources/js/Pages/Diet/DietPlan.jsx
@@ -5,15 +5,17 @@ import PrimaryButton from "@/Components/PrimaryButton";
 import InputLabel from "@/Components/InputLabel";
 import { useForm } from "@inertiajs/react";
 import Feature from "@/Components/Feature";
-import { FaArrowDown } from "react-icons/fa";
+
 import Footer from "@/Components/Footer";
 import { useEffect } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 
-export default function DietPlan({feature, prompt, answer}){
+
+export default function DietPlan({feature, prompt, answer, children}){
     const {data, setData, processing, errors, reset, post} = useForm({
-        prompt: "",
+        prompt: "Hello",
+        answer: "",
         
     });
 
@@ -21,23 +23,27 @@ export default function DietPlan({feature, prompt, answer}){
         e.preventDefault();
         post(route("diet.dietPlan"), {
             onSuccess(){
-                reset();
+                // reset();
             }
         });
-
+        googleText();
     }
+
 
 
     // google AI text generate
     async function googleText(){
-        // const apiKey = "AIzaSyAXZRghqU58r-uFAzMdzqQ-0sfoDbygPSE";
+        // console.log(data.prompt)
+
+        const apiKey = "AIzaSyAXZRghqU58r-uFAzMdzqQ-0sfoDbygPSE";
         const genModel = new GoogleGenerativeAI(apiKey);
         const model = genModel.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const prompt = "give me a diet plan to loss my weight. I am 103 kg, male, my height is 1.86 meter and I want to loss 30kg in 3 months ";
+        const prompt = data.prompt;
 
         const result = await model.generateContent(prompt);
-        console.log(result.response.text());
+        // console.log(result.response.text());
+        data.answer = result.response.text();
 
     }
     useEffect(() => {
@@ -48,18 +54,24 @@ export default function DietPlan({feature, prompt, answer}){
 
     return (
         <Feature feature={feature} answer={answer} subscribedPlan={required_plan}>
-            <h1 className="text-xl dark:text-white text-gray-700 mt-3 ml-8 font-mono">Chat with AI Bot</h1>
+            <h1 className="text-xl dark:text-white text-gray-700 mt-5 ml-8 font-mono">Chat with AI Bot</h1>
+            {
+                data.answer !== null &&(
+                    <div className="text-white text-lg font-mono">{data.answer}</div>
+                )
+            }
 
             <form onSubmit={submit} className="p-8 grid grid-cols-2 gap-3 mt-[500px]">
                 <div>
                     <InputLabel className="text-white font-mono text-lg">Enter Your Message</InputLabel>
-                    <TextInput className="w-full mt-4" placeholder="Message Health Bot"></TextInput>
+                    <TextInput id="prompt" type="text" name="prompt" value={data.prompt} onChange={(e) => setData("prompt", e.target.value) } className="w-full mt-4" placeholder="Message Health Bot" />
                 </div>
 
 
                 <div className="flex items-center mt-9">
                         <PrimaryButton className="ms-4 dark:bg-white dark:text-black" disabled={processing}>Send</PrimaryButton>
                 </div>
+                {children}
             </form>
 
         </Feature>
