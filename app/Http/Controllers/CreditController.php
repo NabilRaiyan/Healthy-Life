6f784fb0d11925aaa27dfb980661d854d0ecc7ec
NabilRaiyan <rraiyan77@gmail.com>
2024-08-22 16:38:29 +0600

    creating credit controller

--- app/Http/Controllers/CreditController.php
+++ app/Http/Controllers/CreditController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\FeatureResource;
 use App\Http\Resources\PackageResource;
 use App\Models\Feature;
 use App\Models\Package;
@@ -15,10 +16,34 @@ class CreditController extends Controller
         $feature = Feature::where('active', true)->get();
         return inertia("Credit/CreditIndex", [
             'packages' => PackageResource::collection($package),
+            'features' => FeatureResource::collection($feature),
+            'success' => session('success'),
+            'error' => session('error'),
         ]);
     }
 
-    public function buyPackage(Package $package){
+    public function buyPackage(Package $package)
+    {
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
+        $checkout_session = $stripe->checkout->sessions->create([
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'usd',
+                        'product_data' =>[
+                            'name' => $package->name . '_' .
+                            $package->duration_days . ' days', 
+                        ],
+                        'unit_amount' => $package->price, 
+                    ],
+                    'quantity' => 1,
+                ]
+                ], 
+                'mode' => 'payment', 
+                'success_url' => route('credit.success', [], true),
+                'cancel_url' => route('credit.cancel', [], true),
+
+                ]);
 
     }
 
