c365dcb7886c53bec571c5366ebb821087df1447
NabilRaiyan <rraiyan77@gmail.com>
2024-08-27 14:54:13 +0600

    creating stripe transaction controller

--- resources/js/Components/PackagePricingCard.jsx
+++ resources/js/Components/PackagePricingCard.jsx
@@ -27,27 +27,20 @@ export default function PackagePricingCard({packages, features}){
                                 </div>
 
                                 <ul role="list" class="mb-8 space-y-4 text-left">
-                                    <li class="flex items-center space-x-3">
+                                    {features.map((feature) => {
+                                        <li key={feature.id} class="flex items-center space-x-3">
                                         <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                        <span>Individual configuration</span>
+                                            <span>{feature.name}</span>
                                     </li>
-                                    <li class="flex items-center space-x-3">
-                                        <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                        <span>No setup, or hidden fees</span>
-                                    </li>
-                                    <li class="flex items-center space-x-3">
-                                        <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                        <span>Team size: <span class="font-semibold">10 developers</span></span>
-                                    </li>
-                                    <li class="flex items-center space-x-3">
-                                        <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                        <span>Premium support: <span class="font-semibold">24 months</span></span>
-                                    </li>
-                                    <li class="flex items-center space-x-3">
-                                        <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                                        <span>Free updates: <span class="font-semibold">24 months</span></span>
-                                    </li>
+                                    })}
                                 </ul>
+
+                                <form action={route("credit.buy", p)} method="POST" className="w-full">
+                                    <input type="hidden" name="_token" value={csrf_token} autoComplete="off"></input>
+                                    <button className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">
+                                        Buy Package
+                                    </button>
+                                </form>
                             </div>
                         ))
                     }
